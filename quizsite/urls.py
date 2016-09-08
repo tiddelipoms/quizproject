cadcca4318615144cf30161c5b968d5283a50217
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-08 11:51:00 +0200

    lagt in comma

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from quiz import views
 urlpatterns = [
 	url(r"^$", views.start),
 	url(r"^quiz/[0-9]+/$",views.quiz),
 	url(r"^quiz/[0-9]+/question/[0-9]+/$", views.question),
-	url(r"^quiz/[0-9]+/results/[0-9]+/$", views.results)
+	url(r"^quiz/[0-9]+/results/[0-9]+/$", views.results),
 ]
