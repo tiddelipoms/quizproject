66e1e05c4db523d6ed364bca347bd59c1b4a5fce
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-08 13:12:13 +0200

    Lagt till testdata på Start och quiz

--- quiz/views.py
+++ quiz/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 quizzes = [
 	{
 		"quiz_number": 1,
-		"nam2": "Klassiska böcker",
+		"name": "Klassiska böcker",
 		"description": "Hur bra kan du dina klassiker?"
 	},
 	{
@@ -19,9 +19,17 @@ quizzes = [
 ]
 
 def start(request):
-	return render(request, "quiz/start.html")
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/start.html", context)
+
+def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[int(quiz_number) -1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 def question(request):
 	return render(request, "quiz/question.html")
 def results(request):
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from quiz import views
 urlpatterns = [
 	url(r"^$", views.start),
-	url(r"^quiz/[0-9]+/$",views.quiz),
+	url(r"^quiz/([0-9]+)/$",views.quiz),
 	url(r"^quiz/[0-9]+/question/[0-9]+/$", views.question),
 	url(r"^quiz/[0-9]+/results/$", views.results),
 ]
