15516e94bbf7c1493e6502fc2a9ac29f115d1a9c
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-08 15:39:46 +0200

    Lagt till radio buttons

--- quiz/views.py
+++ quiz/views.py
@@ -34,9 +34,9 @@ def question(request, quiz_number, question_number):
 	context = {
 		"question_number": question_number,
 		"question": "Hur många bultar har ölandsbron?",
-		"answer1": "12",
-		"answer2": "66 400",
-		"answer3": "7 428 954",
+		"answer1": " 12",
+		"answer2": " 66 400",
+		"answer3": " 7 428 954",
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
