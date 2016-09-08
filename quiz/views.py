c1bbb8c0b0e7af432d09966a75d51e44ea1e3e73
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-08 11:34:21 +0200

    Skapa vyer i quiz/views.py för templates

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def start(request):
+	return render(request, "quiz/start.html")
+def quiz(request):
+	return render(request, "quiz/quiz.html")
+def question(request):
+	return render(request, "quiz/question.html")
+def results(request):
+	return render(request, "quiz/results.html")
