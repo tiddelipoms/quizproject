ca8bc2915fd519b4d15876831df7d2731b09ecb2
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-08 12:47:13 +0200

    Lagt in testdata

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+quizzes = [
+	{
+		"quiz_number": 1,
+		"nam2": "Klassiska böcker",
+		"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+		"name": "Största 1slagen",
+		"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+		"name": "Världens mest kända hackare",
+		"decription": "Hackerhistoria är viktigt, kan du den?"
+	},
+]
+
 def start(request):
 	return render(request, "quiz/start.html")
 def quiz(request):
