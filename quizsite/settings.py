2c895598d11efc6049684311cf7c25c6e98fee82
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-09 13:47:27 +0200

    Uppgraderat databas

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,'db-sqlite3'))
+        )
 }
 
 
