a0b50bc26e6d5b0beb38e10c62fc89839f319427
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-09 16:01:54 +0200

    flytta in plus för att se flera sidor

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -5,8 +5,8 @@ from quiz import views
 urlpatterns = [
 	url(r"^$", views.start, name="start_page"),
 	url(r"^quiz/([0-9]+)/$",views.quiz, name="quiz_page"),
-	url(r"^quiz/([0-9])+/question/([0-9])+/$", views.question, name="question_page"),
-	url(r"^quiz/([0-9])+/results/$", views.results, name="results_page"),
+	url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
+	url(r"^quiz/([0-9]+)/results/$", views.results, name="results_page"),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r"^quiz/([0-9]+)/question/([0-9]+)/answer/$", views.answer,name="answer_page"),
 ]
