384ab67a966444172673e7bab01f4a35c0265044
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-09 13:32:18 +0200

    Uppdatera svar sidan med rätt svar per deltagare

--- quiz/views.py
+++ quiz/views.py
@@ -66,9 +66,16 @@ def answer(request, quiz_number, question_number):
 		return redirect("question_page", quiz_number, question_number + 1)
 
 def results(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	saved_answers = request.session.get(quiz_number, {})
+	num_correct_answer = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+		if correct_answer == answer:
+			num_correct_answer += 1
 	context= {
-		"correct": 12,
-		"total": 20,
-		"quiz_number": quiz_number
+		"correct": num_correct_answer,
+		"total": questions.count(),
 	}
 	return render(request, "quiz/results.html", context)
