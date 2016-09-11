f134b5aedc38254ef13c2b3fe82feb4e4348fc77
tiddelipoms <e.f.lichtenstein@gmail.com>
2016-09-11 21:54:06 +0200

    fix images

--- quiz/views.py
+++ quiz/views.py
@@ -22,6 +22,7 @@ def question(request, quiz_number, question_number):
 	context = {
 		"question_number": question_number,
 		"question": question.question,
+		"question_image": question.question_image,
 		"answer1": question.answer1,
 		"answer2": question.answer2,
 		"answer3": question.answer3,
